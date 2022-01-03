a96b911790c024562b2840caa38b3cf3749b6c7c
markoB1020 <markobudisa99@gmail.com>
2022-01-03 17:20:36 +0100

    serverfinal2

--- loto/pages/izvlacenje.tsx
+++ loto/pages/izvlacenje.tsx
@@ -3,20 +3,24 @@ import Footer from "../components/Footer";
 import nextcookie from "next-cookies";
 import Router from "next/router";
 import fetch from "isomorphic-unfetch";
-import {withAuthSync} from "../utils/auth";
+import { withAuthSync } from "../utils/auth";
 
-const Izvlacenje = (props:any) => {
-    const{
-        numbers=[] //ovo je primer cisto
-    }=props.body;
+//Dugme na koje se poziva get-raffle, ako postoji izvlacenje u bazi, odnosno
+//generisani brojevi, onda se poziva server da vrati korisnikove tikete, 
+//na osnovu cega se utvrdjuje da li je neki tiket dobitan.
+
+const Izvlacenje = (props: any) => {
+    const {
+        numbers = [] //ovo je primer cisto
+    } = props.body;
     var today = new Date();
     var date = today.getFullYear() + '-' + (today.getMonth() + 1) + '-' + today.getDate();
     var tmdate = today.getFullYear() + '-' + (today.getMonth() + 2) + '-' + today.getDate();
-   console.log(numbers)
+    console.log(numbers)
 
     return (
         <div>
-            <Header/>
+            <Header />
             <div className='raffle'>
                 <div className='nextJackpot'><p className='regWeight'>Sledece izvlacenje:</p>
                     <p>{tmdate}</p>
@@ -27,44 +31,44 @@ const Izvlacenje = (props:any) => {
                 <div className='gamePageBalls'>
                     <p className='wnBalls'><span className='balls'>{15}</span><span
                         className='wnDash'>-</span><span
-                        className='balls'>{2}</span><span className='wnDash'>-</span><span
-                        className='balls'>3</span><span
-                        className='wnDash'>-</span><span className='balls'>4</span><span
-                        className='wnDash'>-</span><span
-                        className='balls'>5</span><span className='wnDash sbDash'>-</span><span
-                        className='balls pbBall'>6</span><span className='wnDash'>-</span><span
-                        className='balls multiplier'>7</span></p>
+                            className='balls'>{2}</span><span className='wnDash'>-</span><span
+                                className='balls'>3</span><span
+                                    className='wnDash'>-</span><span className='balls'>4</span><span
+                                        className='wnDash'>-</span><span
+                                            className='balls'>5</span><span className='wnDash sbDash'>-</span><span
+                                                className='balls pbBall'>6</span><span className='wnDash'>-</span><span
+                                                    className='balls multiplier'>7</span></p>
                     <p className='powerball rolloverOrWinners'>0 pobednika </p></div>
             </div>
-            <Footer/>
+            <Footer />
         </div>
     )
 }
 Izvlacenje.getInitialProps = async (ctx: any) => {
     const token = nextcookie(ctx);
     const apiUrl = "http://localhost:3000/get-user-by-username";
-    const apiRaffle="http://localhost:3000/get-raffle";
+    const apiRaffle = "http://localhost:3000/get-raffle";
 
-   const redirectOnError = () =>
+    const redirectOnError = () =>
         typeof window !== "undefined"
             ? Router.push("/prijava")
-            : ctx.res.writeHead(302, {Location: "/prijava"}).end();
+            : ctx.res.writeHead(302, { Location: "/prijava" }).end();
 
     const response = await fetch(apiUrl, {
         method: 'POST',
         headers: {
             'Access-Control-Allow-Origin': '*'
         },
-        body: JSON.stringify({token}),
+        body: JSON.stringify({ token }),
     });
     const response2 = await fetch(apiRaffle, {
         method: 'POST',
         headers: {
             'Access-Control-Allow-Origin': '*'
         },
-        body: JSON.stringify({token}),
+        body: JSON.stringify({ token }),
     });
-    if (response.status==200 && response2.status==200) {
+    if (response.status == 200 && response2.status == 200) {
         return await response.json();
     } else {
         return await redirectOnError();
